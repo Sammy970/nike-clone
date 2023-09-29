4271ddb41216bab33159b67aef4a34cc9d75259b
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-09-29 18:36:09 +0530

    Completed Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ import {
   Footer,
 } from "./sections";
 
+import Nav from "./components/Nav";
+
 const App = () => {
   return (
     <main className="relative">
-      {/* <Nav /> */}
-      <section className="xl:padding-1 wide:padding-r padding-b">
+      <Nav />
+      <section className="xl:padding-l wide:padding-r padding-b">
         <Hero />
       </section>
       <section className="padding">
