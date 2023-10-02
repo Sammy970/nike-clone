ece7cda4a835a8766756f480a463d9d96fca96e6
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-10-02 13:37:07 +0530

    Added Services Section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,5 +1,14 @@
+import ServiceCard from "../components/ServiceCard";
+import { services } from "../constants";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((serivce) => (
+        <ServiceCard key={serivce.label} {...serivce} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
